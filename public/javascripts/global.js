b5f29f9186671aa5be9e34795753059f5ea65a27
Cobbleopolis <cobbleopolis@gmail.com>
2015-12-04 12:12:46 -0500

    Nav button animation is now correct

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,16 +2,32 @@ var isNavShown = false;
 var nav = null;
 var content = null;
 var navButton = null;
+var navLabel = $( "<span id=\"navLabel\">&nbsp;&nbsp;&nbsp;Navigation</span>" );
+navLabel.fadeOut();
 
 
 $(function() {
     content = $("#content");
     nav = $("#nav");
     navButton = $("#navButton");
-    //nav.css("left", nav.width());
-    //nav.css("z-index", "auto");
+
+    navButton.append(navLabel);
+    navButton.css("height", navButton.height());
+
     navButton.click(function() {
-        toggleNav()
+        toggleNav();
+    });
+    navButton.hover(
+        function() {
+            navLabel.stop().animate({opacity: "toggle", width: "toggle"});
+        }, function() {
+            navLabel.stop().animate({opacity: "toggle", width: "toggle"});
+
+        }
+    );
+    content.click(function() {
+        if (isNavShown)
+            toggleNav();
     });
 });
 
