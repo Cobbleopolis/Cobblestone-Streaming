48af9f79ccc9122b71c1a32788e232d435cad629
Cobbleopolis <cobbleopolis@gmail.com>
2015-12-10 13:24:25 -0500

    Navigation bar now reads correctly w/ icons

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -6,7 +6,7 @@ var navLabel = $( "<span id=\"navLabel\">&nbsp;&nbsp;&nbsp;Navigation</span>" );
 navLabel.fadeOut();
 
 
-$(function() {
+$(document).ready( function() {
     content = $("#content");
     nav = $("#nav");
     navButton = $("#navButton");
@@ -14,25 +14,67 @@ $(function() {
     navButton.append(navLabel);
     navButton.css("height", navButton.height());
 
-    navButton.click(function() {
+    navButton.click(function () {
         toggleNav();
     });
     navButton.hover(
-        function() {
+        function () {
             navLabel.stop().animate({opacity: "toggle", width: "toggle"});
-        }, function() {
+        }, function () {
             navLabel.stop().animate({opacity: "toggle", width: "toggle"});
 
         }
     );
-    content.click(function() {
+    content.click(function () {
         if (isNavShown)
             toggleNav();
     });
+    $(".folder").click(function (event) {
+        if (this == event.target) {
+            var jThis = $(this);
+            var i = jThis.find($("i"));
+
+            if (i.hasClass("fa-folder")) {
+                i.removeClass("fa-folder");
+                i.addClass("fa-folder-open");
+            } else if (i.hasClass("fa-folder-open")) {
+                i.removeClass("fa-folder-open");
+                i.addClass("fa-folder");
+            }
+
+            var child = $(jThis.siblings()[0]);
+            child.stop().animate({height: "toggle"},
+                {
+                    start: function() {
+                        content.animate({"left": nav.outerWidth()}, 350);
+                        navButton.animate({"left": nav.outerWidth()}, 350);
+                    },
+                    complete: function() {
+                        content.animate({"left": nav.outerWidth()}, 350);
+                        navButton.animate({"left": nav.outerWidth()}, 350);
+                    }
+                }
+            );
+
+            //child.stop().animate({height: "toggle"},
+            //    {
+            //        duration: 400,
+            //        progress: function(animation, progress, remainingMs) {
+            //            var left = (progress / 400) * nav.outerWidth();
+            //
+            //            content.animate({"left": "+=" + left}, 1);
+            //            navButton.animate({"left": "+=" + left}, 1);
+            //        }
+            //    }
+            //);
+
+        }
+    });
+
 });
 
 function openNav() {
-    console.log("OPEN");
+    //console.log("OPEN");
     if (!isNavShown) {
         content.animate({"left": nav.outerWidth()}, 350);
         navButton.animate({"left": nav.outerWidth()}, 350);
@@ -41,7 +83,7 @@ function openNav() {
 }
 
 function closeNav() {
-    console.log("CLOSE");
+    //console.log("CLOSE");
     if (isNavShown) {
         content.animate({"left": "0"}, 350);
         navButton.animate({"left": "0"}, 350);
